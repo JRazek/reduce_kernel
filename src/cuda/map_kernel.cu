1d73373e960e7d4ba6921cb1bb8a67ed4346882c
JRazek <jakub.razek2@gmail.com>
2024-04-21 01:24:01 +0200

    bound check kernel

--- src/cuda/map_kernel.cu
+++ src/cuda/map_kernel.cu
@@ -3,10 +3,14 @@
 #include <cuda_runtime_api.h>
 
 template <typename T>
-__device__ auto map_offsets_in_place(T *data, const std::size_t *idx_to_offsets)
-    -> void {
+__device__ auto map_offsets_in_place(T *data, const std::size_t *idx_to_offsets,
+                                     std::size_t size) -> void {
   auto tid = threadIdx.x;
   auto idx = blockIdx.x * blockDim.x + threadIdx.x; // # in grid.
+
+  if (idx >= size) {
+    return;
+  }
 
   auto tensor_idx = idx_to_offsets[idx]; // offset in data.
   auto input = data[tensor_idx];
@@ -20,8 +24,8 @@ __device__ auto map_offsets_in_place(T *data, const std::size_t *idx_to_offsets)
 
 #define EXTERN(T, SUFFIX)                                                      \
   extern "C" __global__ void map_offsets_##SUFFIX(                             \
-      T *in, const std::size_t *offsets) {                                     \
-    map_offsets_in_place(in, offsets);                                         \
+      T *in, const std::size_t *offsets, std::size_t size) {                   \
+    map_offsets_in_place(in, offsets, size);                                   \
   }
 
 // actually they are not always f32/f64 by cpp standard but for simplicity -
